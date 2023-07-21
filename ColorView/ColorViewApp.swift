cdfa707a7b03821e47a7203decfc25a13319dae0
Alexey Turulin <repakuku@icloud.com>
2023-07-21 17:31:56 -0400

    Initial Commit

---
+++ ColorView/ColorViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorViewApp.swift
+//  ColorView
+//
+//  Created by Алексей Турулин on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
