836de5842edd7ae40aee5c92199a85a913e01791
Alexey Turulin <repakuku@icloud.com>
2023-07-23 15:58:54 -0400

    Homework

--- ColorView/ContentView.swift
+++ ColorView/ContentView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum TextField {
+        case redTextField
+        case greenTextField
+        case blueTextField
+    }
+    
     @State private var redSliderValue = Double.random(in: 0...255)
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
     
-    @FocusState private var keyboardPresented: Bool
+    @State private var alertPresented = false
+    
+    @FocusState private var focusedTextField: TextField?
     
     var body: some View {
         ZStack {
@@ -21,25 +29,79 @@ struct ContentView: View {
             VStack(spacing: 20) {
                 ColorView(redValue: $redSliderValue, greenValue: $greenSliderValue, blueValue: $blueSliderValue)
                 SliderView(value: $redSliderValue, tintColor: .red)
+                    .focused($focusedTextField, equals: .redTextField)
                 SliderView(value: $greenSliderValue, tintColor: .green)
+                    .focused($focusedTextField, equals: .greenTextField)
                 SliderView(value: $blueSliderValue, tintColor: .blue)
+                    .focused($focusedTextField, equals: .blueTextField)
                 Spacer()
             }
-            .focused($keyboardPresented)
             .padding()
         }
         .toolbar {
-            ToolbarItem(placement: .keyboard) {
+            ToolbarItemGroup(placement: .keyboard) {
+                Button("Up") {
+                    switchTextField(up: true)
+                }
+                Button("Down") {
+                    switchTextField(up: false)
+                }
+                
+                Spacer()
                 Button("Done") {
-                    keyboardPresented.toggle()
+                    focusedTextField = nil
                     checkValue()
+                }
+                .alert("Wrong Format", isPresented: $alertPresented, actions: {}) {
+                    Text("Please enter value from 0 to 255")
                 }
             }
         }
     }
     
+    private func switchTextField(up: Bool) {
+        if up {
+            switch focusedTextField {
+            case .none:
+                break
+            case .redTextField:
+                focusedTextField = .blueTextField
+            case .greenTextField:
+                focusedTextField = .redTextField
+            case .blueTextField:
+                focusedTextField = .greenTextField
+            }
+        } else {
+            switch focusedTextField {
+            case .none:
+                break
+            case .redTextField:
+                focusedTextField = .greenTextField
+            case .greenTextField:
+                focusedTextField = .blueTextField
+            case .blueTextField:
+                focusedTextField = .redTextField
+            }
+        }
+    }
+    
     private func checkValue() {
+        var value = 0.0
         
+        switch focusedTextField {
+        case .none:
+            break
+        case .redTextField:
+            value = redSliderValue
+        case .greenTextField:
+            value = greenSliderValue
+        case .blueTextField:
+            value = blueSliderValue
+        }
+        
+        if !(0...255).contains(value) {
+            alertPresented = true
+        }
     }
 }
 
